ac43950da6d9a9d03cab4d17db76831ec01109a0
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2022-11-15 18:42:10 -0300

    praticas

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples!
+    console.log(`Você foi multado por ultrapassar a velocidade permitida!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Você está vivendo no ${pais}`)
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
