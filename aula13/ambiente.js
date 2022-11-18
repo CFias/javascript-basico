90bb92f4b06c9b22540fb94348bf8fcd7122430b
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2022-11-18 18:11:19 -0300

    do/while

---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 1 
+do {
+    console.log(`Passso ${c}`)
+    c++
+} while (c <= 6) 
+
+
+
+
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+
+
+
+/*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
