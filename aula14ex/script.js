c80f8f4f66d476d888e01f367dce47af18879a5b
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2022-11-18 20:44:05 -0300

    1

---
+++ aula14ex/script.js
@@ -0,0 +1,8 @@
+function clicar() {
+    var ini = document.querySelector('#iniciar')
+    var fim = document.querySelector('#fim')
+    var pas = document.querySelector('#passo')
+    var cont = Number(ini.value)
+    var res = cont
+    res.innerHTML = ''
+}
