0116aae83e8b9668d31c8d22404887c25f6ff830
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2022-11-19 21:44:00 -0300

    for

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -1,8 +1,32 @@
-function clicar() {
-    var ini = document.querySelector('#iniciar')
-    var fim = document.querySelector('#fim')
-    var pas = document.querySelector('#passo')
-    var cont = Number(ini.value)
-    var res = cont
-    res.innerHTML = ''
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        // window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                 res.innerHTML += ` ${c} \u{1F449} `
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`     
+    } 
 }
